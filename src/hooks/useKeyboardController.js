be640c01b2ff06d3c618df0cb3c954eb6a14c5c4
Ty <lastcanal@gmail.com>
2019-11-02 10:52:09 -0400

    Move keyboard controller event extraction to hook.

--- src/hooks/useKeyboardController.js
+++ src/hooks/useKeyboardController.js
@@ -1,6 +1,73 @@
 import { useEffect } from 'react'
 
-import { handleKeyboardAction } from '../actions'
+import {
+  nextPost,
+  previousPost,
+  configTogglePlay,
+  configToggleAutoplay,
+  configToggleFullscreen,
+  playerScanBackwards,
+  playerScanForwards,
+  playerVolumeUp,
+  playerVolumeDown,
+  playerJumpTo,
+} from '../actions'
+
+
+export const handleKeyboardAction = event => (dispatch, getState) => {
+  const { config } = getState()
+
+  if (!config.keyboardControls) return
+
+  switch (event.key) {
+    case '.':
+    case '>':
+      return dispatch(nextPost())
+    case 'N':
+    case 'n':
+      return event.shiftKey ? dispatch(nextPost()) : void (0)
+    case ',':
+    case '<':
+      return dispatch(previousPost())
+    case 'P':
+    case 'p':
+      return event.shiftKey ? dispatch(previousPost()) : void (0)
+    case ' ':
+    case 'Enter':
+      if (!event.shiftKey) event.preventDefault()
+      return dispatch(configTogglePlay())
+    case 'k':
+      return dispatch(configTogglePlay())
+    case 'a':
+      return dispatch(configToggleAutoplay())
+    case 'f':
+      return dispatch(configToggleFullscreen())
+    case 'ArrowLeft':
+      return dispatch(playerScanBackwards(5))
+    case 'ArrowRight':
+      return dispatch(playerScanForwards(5))
+    case 'ArrowUp':
+      if (!event.shiftKey) event.preventDefault()
+      return dispatch(playerVolumeUp())
+    case 'ArrowDown':
+      if (!event.shiftKey) event.preventDefault()
+      return dispatch(playerVolumeDown())
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      const jumpTo = parseInt(event.key, 10)
+      return dispatch(playerJumpTo(jumpTo))
+    default:
+      return
+  }
+}
 
 const useKeyboardController = ({ dispatch }) => {
   const handleKeyDown = (event) => {
