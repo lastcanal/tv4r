24d07a5d1444144a8b6e25a0ac6af800e3922eb2
Ty <lastcanal@gmail.com>
2019-11-03 13:07:52 -0500

    Propogate resize only if width  changed
    Related to IOS sending extra resize events when hiding the menu

--- src/hooks/useLayoutDimensionTracker.js
+++ src/hooks/useLayoutDimensionTracker.js
@@ -4,8 +4,11 @@ import debounce from 'lodash.debounce'
 const useLayoutDimensionTracker = ({ isFullscreen, isFetching }) => {
   const menuRef = useRef()
 
+  const [height, setHeight] = useState(0)
   const [menuHeight, setMenuHeight] = useState(0)
   const [menuOffsetHeight, setMenuOffsetHeight] = useState(0)
+  const [menuWidth, setMenuWidth] = useState(document.innerWidth)
+  const [orientation, setOrientation] = useState(0)
 
   useLayoutEffect(() => {
     const { current } = menuRef
@@ -13,29 +16,44 @@ const useLayoutDimensionTracker = ({ isFullscreen, isFetching }) => {
       const box = current.getBoundingClientRect()
       setMenuHeight(box.height)
     }
-  }, [menuRef])
+  }, [menuRef, orientation])
 
   useEffect(() => {
     setMenuOffsetHeight(isFullscreen ? 0 : menuHeight)
-  }, [isFullscreen, menuHeight])
+  }, [isFullscreen, menuHeight, orientation])
 
   const calculateHeight = () => (
     (window.innerHeight - menuOffsetHeight)
   )
 
-  const [height, setHeight] = useState(calculateHeight())
-
-  useEffect(() => {
+  useLayoutEffect(() => {
     setHeight(calculateHeight())
-  }, [menuOffsetHeight])
+  }, [menuOffsetHeight, orientation])
 
   const onResize = debounce(() => {
-    setHeight(calculateHeight())
+    // prevent mobile safari/chrome from flailing
+    if (menuWidth !== document.innerWidth) {
+      setHeight(calculateHeight())
+      setMenuWidth(document.innerWidth)
+    }
   }, 100)
+
+  const onOrientationChange = () => {
+    if (window.orientation) {
+      setOrientation(window.orientation)
+    }
+  }
 
   useEffect(() => {
     window.addEventListener('resize', onResize)
     return () => (window.removeEventListener('resize', onResize))
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener('orientationchange', onOrientationChange)
+    return () => (
+      window.removeEventListener('orientationchange', onOrientationChange)
+    )
   }, [])
 
   useEffect(() => {
