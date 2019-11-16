39e1e9be3c9e9875dba20ff49932735a6f0799d7
Ty <lastcanal@gmail.com>
2019-11-16 18:45:53 -0500

    Deploy

---
+++ precache-manifest.52e40b097d721ff07eb4f154fb781045.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372a0665cd7ac993adbbe549071387e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f612a1559f552a01861",
+    "url": "/static/js/2.2892fbed.chunk.js"
+  },
+  {
+    "revision": "de936efcac98986e1882",
+    "url": "/static/js/main.03f071b0.chunk.js"
+  },
+  {
+    "revision": "f7f2c8279fcd6bf32f28",
+    "url": "/static/js/runtime~main.015c3f6f.js"
+  }
+]);
